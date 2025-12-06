9cd9bf8fd3c8824c5734e315d3654221ea193457
ZoIman <maksymnagornyi@ukr.net>
2025-12-06 16:49:27 +0200

    zxc

--- script.js
+++ script.js
@@ -16,7 +16,7 @@
 
 // xhr.send(data);
 const url = "https://zovzovzov-48aa.restdb.io/rest/product"
-const api_key = "d453156b01d270172ab746f52cb6897a1d671"
+const api_key = "69343d921c64b97e52dde62f"
 
 const my_header = {
     "Content-Type": 'application/json',
