c1e906596d5f3f2e79ed9cb5494a1e530a136d7c
ZoIman <maksymnagornyi@ukr.net>
2025-12-06 16:09:40 +0200

    zxc

---
+++ script.js
@@ -0,0 +1,18 @@
+// var data = null;
+
+// var xhr = new XMLHttpRequest();
+// xhr.withCredentials = false;
+
+// xhr.addEventListener("readystatechange", function () {
+//   if (this.readyState === 4) {
+//     console.log(this.responseText);
+//   }
+// });
+
+// xhr.open("GET", "https://zovzovzov-48aa.restdb.io/rest/product");
+// xhr.setRequestHeader("content-type", "application/json");
+// xhr.setRequestHeader("x-apikey", "<your CORS apikey here>");
+// xhr.setRequestHeader("cache-control", "no-cache");
+
+// xhr.send(data);
+const url = "https://zovzovzov-48aa.restdb.io/rest/product"
